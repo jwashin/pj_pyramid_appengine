50f39a7469bee3ed2e4c8ec658283ea7aacb08c6
Jim Washington <jwashin@users.sourceforge.net>
2012-03-24 07:00:15 -0400

    initial commit

--- pyramid_appengine/scaffolds/starter/src/+package+/+package+/app.py
+++ pyramid_appengine/scaffolds/starter/src/+package+/+package+/app.py
@@ -3,6 +3,7 @@ from resources import Root
 import views
 import pyramid_jinja2
 import os
+
 __here__ = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -15,4 +16,8 @@ def main(global_config, **settings):
                     context=Root,
                     renderer='mytemplate.jinja2')
     config.add_static_view(name='static', path=os.path.join(__here__, 'static'))
+    config.include('pyramid_rpc.jsonrpc')
+
+    config.add_jsonrpc_endpoint('echoservice', '/echo')
+    config.scan()
     return config.make_wsgi_app()
